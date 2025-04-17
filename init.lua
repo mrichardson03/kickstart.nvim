25d0779406c901853193cb70dc4531cab948c5dd
Michael Richardson <8782+mrichardson03@users.noreply.github.com>
2025-04-17 10:42:30 -0400

    feat: refresh neotree on buffer leave

--- init.lua
+++ init.lua
@@ -214,6 +214,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Refresh Neotree view on buffer leave
+vim.api.nvim_create_autocmd('BufLeave', {
+  pattern = { '*lazygit*' },
+  group = vim.api.nvim_create_augroup('git_refresh_neotree', { clear = true }),
+  callback = function()
+    require('neo-tree.sources.filesystem.commands').refresh(require('neo-tree.sources.manager').get_state 'filesystem')
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
