bfbc79a16ba576de54be1f3b8eb1f0f3fe5713bd
Michael Richardson <8782+mrichardson03@users.noreply.github.com>
2025-04-21 13:43:46 -0400

    feat: add python lsp, formatting

--- init.lua
+++ init.lua
@@ -699,6 +699,7 @@ require('lazy').setup({
             },
           },
         },
+        pylsp = {},
         terraformls = {},
       }
 
@@ -718,6 +719,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'black',
+        'isort',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
